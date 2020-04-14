e945fa299f3a6a044cf4db0fca617a619bd52bcf
AlbanBD <albanbezieau@gmail.com>
2020-04-14 15:56:58 +0200

    Projet Unity

---
+++ Roll a Ball/Assets/Script/Rotate.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+
+    void Update ()
+    {
+        transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+    }
+}
