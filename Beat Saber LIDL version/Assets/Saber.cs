2e8d457e8e29263e3e761094cc8ee1c89ecfb67c
AlbanBD <albanbezieau@gmail.com>
2020-06-29 08:33:41 +0200

    Ajout du projet Beat Saber

---
+++ Beat Saber LIDL version/Assets/Saber.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Saber : MonoBehaviour
+{
+    public LayerMask layer;
+    private Vector3 previousPOS;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, transform.forward, out hit, 1, layer))
+        {
+            if (Vector3.Angle(transform.position-previousPOS,hit.transform.up)>130)
+            {
+                Destroy(hit.transform.gameObject);
+            }
+        }
+        previousPOS = transform.position;
+    }
+}
