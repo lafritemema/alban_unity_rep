febd43154a5ba2859f1944f0a67cfadc36dd2f4a
AlbanBD <albanbezieau@gmail.com>
2020-06-29 09:15:28 +0200

    Ajout Project Setup

---
+++ Setup/Assets/Script/Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class Controller : MonoBehaviour
+{
+    public XRController left;
+    public XRController right;
+    public InputHelpers.Button teleportActivation;
+    public float activation = 0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (left)
+        {
+            left.gameObject.SetActive(CheckIfActivated(left));
+        }
+
+        if (right)
+        {
+            right.gameObject.SetActive(CheckIfActivated(right));
+        }
+    }
+
+    public bool CheckIfActivated(XRController controller)
+    {
+        InputHelpers.IsPressed(controller.inputDevice, teleportActivation, out bool isActivated, activation);
+        return isActivated;
+    }
+}
